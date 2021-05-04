c5017f8e42f7e6a6b4d550fbfbe8cd72a2af9e27
nabujjal <nabujjal@gmail.com>
2021-05-04 23:56:32 +0530

    Header-Body-Footer-Initial1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <h1>Welcome to Just Shop</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Just Shop</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <p>
+        Copyright <span id="copy">&copy;</span> All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
